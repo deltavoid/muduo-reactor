5dd2fca9fd7e2c07979bdd529f6c5bc74dbcf757
zqy <850074816@qq.com>
2019-07-30 14:40:16 +0800

    init common, s01, s09

---
+++ s09/test2.cc
@@ -0,0 +1,18 @@
+#include "reactor/EventLoop.h"
+#include <common/Thread.h>
+
+muduo::EventLoop* g_loop;
+
+void threadFunc()
+{
+  g_loop->loop();
+}
+
+int main()
+{
+  muduo::EventLoop loop;
+  g_loop = &loop;
+  muduo::Thread t(threadFunc);
+  t.start();
+  t.join();
+}

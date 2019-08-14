43595b3038a5c72b4e34835fe31b609da98e4038
zqy <850074816@qq.com>
2019-08-14 15:08:57 +0800

    update s01 format

--- s01/reactor/Channel.cc
+++ s01/reactor/Channel.cc
@@ -7,7 +7,7 @@
 
 #include "Channel.h"
 #include "EventLoop.h"
-#include  <common/Logging.h>
+#include <common/Logging.h>
 
 #include <sstream>
 
@@ -19,33 +19,40 @@ const int Channel::kNoneEvent = 0;
 const int Channel::kReadEvent = POLLIN | POLLPRI;
 const int Channel::kWriteEvent = POLLOUT;
 
-Channel::Channel(EventLoop* loop, int fdArg)
-  : loop_(loop),
-    fd_(fdArg),
-    events_(0),
-    revents_(0),
-    index_(-1)
+Channel::Channel(EventLoop *loop, int fdArg)
+    : loop_(loop),
+      fd_(fdArg),
+      events_(0),
+      revents_(0),
+      index_(-1)
 {
 }
 
 void Channel::update()
 {
-  loop_->updateChannel(this);
+    loop_->updateChannel(this);
 }
 
 void Channel::handleEvent()
 {
-  if (revents_ & POLLNVAL) {
-    LOG_WARN << "Channel::handle_event() POLLNVAL";
-  }
+    if (revents_ & POLLNVAL)
+    {
+        LOG_WARN << "Channel::handle_event() POLLNVAL";
+    }
 
-  if (revents_ & (POLLERR | POLLNVAL)) {
-    if (errorCallback_) errorCallback_();
-  }
-  if (revents_ & (POLLIN | POLLPRI | POLLRDHUP)) {
-    if (readCallback_) readCallback_();
-  }
-  if (revents_ & POLLOUT) {
-    if (writeCallback_) writeCallback_();
-  }
+    if (revents_ & (POLLERR | POLLNVAL))
+    {
+        if (errorCallback_)
+            errorCallback_();
+    }
+    if (revents_ & (POLLIN | POLLPRI | POLLRDHUP))
+    {
+        if (readCallback_)
+            readCallback_();
+    }
+    if (revents_ & POLLOUT)
+    {
+        if (writeCallback_)
+            writeCallback_();
+    }
 }

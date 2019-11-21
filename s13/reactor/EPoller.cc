f25c76f7d604782c776717271d95c90bcf301aa7
zqy <850074816@qq.com>
2019-11-21 16:23:47 +0800

    update s13

--- s13/reactor/EPoller.cc
+++ s13/reactor/EPoller.cc
@@ -10,7 +10,6 @@
 #include "Channel.h"
 #include <common/Logging.h>
 
-
 #include <boost/static_assert.hpp>
 
 #include <assert.h>
@@ -34,149 +33,148 @@ namespace
 const int kNew = -1;
 const int kAdded = 1;
 const int kDeleted = 2;
-}
+} // namespace
 
-EPoller::EPoller(EventLoop* loop)
-  : ownerLoop_(loop),
-    epollfd_(::epoll_create1(EPOLL_CLOEXEC)),
-    events_(kInitEventListSize)
+EPoller::EPoller(EventLoop *loop)
+    : ownerLoop_(loop),
+      epollfd_(::epoll_create1(EPOLL_CLOEXEC)),
+      events_(kInitEventListSize)
 {
-  if (epollfd_ < 0)
-  {
-    LOG_SYSFATAL << "EPoller::EPoller";
-  }
+    if (epollfd_ < 0)
+    {
+        LOG_SYSFATAL << "EPoller::EPoller";
+    }
 }
 
 EPoller::~EPoller()
 {
-  ::close(epollfd_);
+    ::close(epollfd_);
 }
 
-Timestamp EPoller::poll(int timeoutMs, ChannelList* activeChannels)
+Timestamp EPoller::poll(int timeoutMs, ChannelList *activeChannels)
 {
-  int numEvents = ::epoll_wait(epollfd_,
-                               &*events_.begin(),
-                               static_cast<int>(events_.size()),
-                               timeoutMs);
-  Timestamp now(Timestamp::now());
-  if (numEvents > 0)
-  {
-    LOG_TRACE << numEvents << " events happended";
-    fillActiveChannels(numEvents, activeChannels);
-    if (implicit_cast<size_t>(numEvents) == events_.size())
+    int numEvents = ::epoll_wait(epollfd_,
+                                 &*events_.begin(),
+                                 static_cast<int>(events_.size()),
+                                 timeoutMs);
+    Timestamp now(Timestamp::now());
+    if (numEvents > 0)
     {
-      events_.resize(events_.size()*2);
+        LOG_TRACE << numEvents << " events happended";
+        fillActiveChannels(numEvents, activeChannels);
+        if (implicit_cast<size_t>(numEvents) == events_.size())
+        {
+            events_.resize(events_.size() * 2);
+        }
     }
-  }
-  else if (numEvents == 0)
-  {
-    LOG_TRACE << " nothing happended";
-  }
-  else
-  {
-    LOG_SYSERR << "EPoller::poll()";
-  }
-  return now;
+    else if (numEvents == 0)
+    {
+        LOG_TRACE << " nothing happended";
+    }
+    else
+    {
+        LOG_SYSERR << "EPoller::poll()";
+    }
+    return now;
 }
 
 void EPoller::fillActiveChannels(int numEvents,
-                                 ChannelList* activeChannels) const
+                                 ChannelList *activeChannels) const
 {
-  assert(implicit_cast<size_t>(numEvents) <= events_.size());
-  for (int i = 0; i < numEvents; ++i)
-  {
-    Channel* channel = static_cast<Channel*>(events_[i].data.ptr);
+    assert(implicit_cast<size_t>(numEvents) <= events_.size());
+    for (int i = 0; i < numEvents; ++i)
+    {
+        Channel *channel = static_cast<Channel *>(events_[i].data.ptr);
 #ifndef NDEBUG
-    int fd = channel->fd();
-    ChannelMap::const_iterator it = channels_.find(fd);
-    assert(it != channels_.end());
-    assert(it->second == channel);
+        int fd = channel->fd();
+        ChannelMap::const_iterator it = channels_.find(fd);
+        assert(it != channels_.end());
+        assert(it->second == channel);
 #endif
-    channel->set_revents(events_[i].events);
-    activeChannels->push_back(channel);
-  }
+        channel->set_revents(events_[i].events);
+        activeChannels->push_back(channel);
+    }
 }
 
-void EPoller::updateChannel(Channel* channel)
+void EPoller::updateChannel(Channel *channel)
 {
-  assertInLoopThread();
-  LOG_TRACE << "fd = " << channel->fd() << " events = " << channel->events();
-  const int index = channel->index();
-  if (index == kNew || index == kDeleted)
-  {
-    // a new one, add with EPOLL_CTL_ADD
-    int fd = channel->fd();
-    if (index == kNew)
+    assertInLoopThread();
+    LOG_TRACE << "fd = " << channel->fd() << " events = " << channel->events();
+    const int index = channel->index();
+    if (index == kNew || index == kDeleted)
     {
-      assert(channels_.find(fd) == channels_.end());
-      channels_[fd] = channel;
+        // a new one, add with EPOLL_CTL_ADD
+        int fd = channel->fd();
+        if (index == kNew)
+        {
+            assert(channels_.find(fd) == channels_.end());
+            channels_[fd] = channel;
+        }
+        else // index == kDeleted
+        {
+            assert(channels_.find(fd) != channels_.end());
+            assert(channels_[fd] == channel);
+        }
+        channel->set_index(kAdded);
+        update(EPOLL_CTL_ADD, channel);
     }
-    else // index == kDeleted
+    else
     {
-      assert(channels_.find(fd) != channels_.end());
-      assert(channels_[fd] == channel);
+        // update existing one with EPOLL_CTL_MOD/DEL
+        int fd = channel->fd();
+        (void)fd;
+        assert(channels_.find(fd) != channels_.end());
+        assert(channels_[fd] == channel);
+        assert(index == kAdded);
+        if (channel->isNoneEvent())
+        {
+            update(EPOLL_CTL_DEL, channel);
+            channel->set_index(kDeleted);
+        }
+        else
+        {
+            update(EPOLL_CTL_MOD, channel);
+        }
     }
-    channel->set_index(kAdded);
-    update(EPOLL_CTL_ADD, channel);
-  }
-  else
-  {
-    // update existing one with EPOLL_CTL_MOD/DEL
+}
+
+void EPoller::removeChannel(Channel *channel)
+{
+    assertInLoopThread();
     int fd = channel->fd();
-    (void)fd;
+    LOG_TRACE << "fd = " << fd;
     assert(channels_.find(fd) != channels_.end());
     assert(channels_[fd] == channel);
-    assert(index == kAdded);
-    if (channel->isNoneEvent())
+    assert(channel->isNoneEvent());
+    int index = channel->index();
+    assert(index == kAdded || index == kDeleted);
+    size_t n = channels_.erase(fd);
+    (void)n;
+    assert(n == 1);
+
+    if (index == kAdded)
     {
-      update(EPOLL_CTL_DEL, channel);
-      channel->set_index(kDeleted);
+        update(EPOLL_CTL_DEL, channel);
     }
-    else
-    {
-      update(EPOLL_CTL_MOD, channel);
-    }
-  }
+    channel->set_index(kNew);
 }
 
-void EPoller::removeChannel(Channel* channel)
+void EPoller::update(int operation, Channel *channel)
 {
-  assertInLoopThread();
-  int fd = channel->fd();
-  LOG_TRACE << "fd = " << fd;
-  assert(channels_.find(fd) != channels_.end());
-  assert(channels_[fd] == channel);
-  assert(channel->isNoneEvent());
-  int index = channel->index();
-  assert(index == kAdded || index == kDeleted);
-  size_t n = channels_.erase(fd);
-  (void)n;
-  assert(n == 1);
-
-  if (index == kAdded)
-  {
-    update(EPOLL_CTL_DEL, channel);
-  }
-  channel->set_index(kNew);
-}
-
-void EPoller::update(int operation, Channel* channel)
-{
-  struct epoll_event event;
-  bzero(&event, sizeof event);
-  event.events = channel->events();
-  event.data.ptr = channel;
-  int fd = channel->fd();
-  if (::epoll_ctl(epollfd_, operation, fd, &event) < 0)
-  {
-    if (operation == EPOLL_CTL_DEL)
+    struct epoll_event event;
+    bzero(&event, sizeof event);
+    event.events = channel->events();
+    event.data.ptr = channel;
+    int fd = channel->fd();
+    if (::epoll_ctl(epollfd_, operation, fd, &event) < 0)
     {
-      LOG_SYSERR << "epoll_ctl op=" << operation << " fd=" << fd;
+        if (operation == EPOLL_CTL_DEL)
+        {
+            LOG_SYSERR << "epoll_ctl op=" << operation << " fd=" << fd;
+        }
+        else
+        {
+            LOG_SYSFATAL << "epoll_ctl op=" << operation << " fd=" << fd;
+        }
     }
-    else
-    {
-      LOG_SYSFATAL << "epoll_ctl op=" << operation << " fd=" << fd;
-    }
-  }
 }
-
--- s13/reactor/EPoller.h
+++ s13/reactor/EPoller.h
@@ -27,40 +27,40 @@ class Channel;
 /// This class doesn't own the Channel objects.
 class EPoller : boost::noncopyable
 {
- public:
-  typedef std::vector<Channel*> ChannelList;
+public:
+    typedef std::vector<Channel *> ChannelList;
 
-  EPoller(EventLoop* loop);
-  ~EPoller();
+    EPoller(EventLoop *loop);
+    ~EPoller();
 
-  /// Polls the I/O events.
-  /// Must be called in the loop thread.
-  Timestamp poll(int timeoutMs, ChannelList* activeChannels);
+    /// Polls the I/O events.
+    /// Must be called in the loop thread.
+    Timestamp poll(int timeoutMs, ChannelList *activeChannels);
 
-  /// Changes the interested I/O events.
-  /// Must be called in the loop thread.
-  void updateChannel(Channel* channel);
-  /// Remove the channel, when it destructs.
-  /// Must be called in the loop thread.
-  void removeChannel(Channel* channel);
+    /// Changes the interested I/O events.
+    /// Must be called in the loop thread.
+    void updateChannel(Channel *channel);
+    /// Remove the channel, when it destructs.
+    /// Must be called in the loop thread.
+    void removeChannel(Channel *channel);
 
-  void assertInLoopThread() { ownerLoop_->assertInLoopThread(); }
+    void assertInLoopThread() { ownerLoop_->assertInLoopThread(); }
 
- private:
-  static const int kInitEventListSize = 16;
+private:
+    static const int kInitEventListSize = 16;
 
-  void fillActiveChannels(int numEvents,
-                          ChannelList* activeChannels) const;
-  void update(int operation, Channel* channel);
+    void fillActiveChannels(int numEvents,
+                            ChannelList *activeChannels) const;
+    void update(int operation, Channel *channel);
 
-  typedef std::vector<struct epoll_event> EventList;
-  typedef std::map<int, Channel*> ChannelMap;
+    typedef std::vector<struct epoll_event> EventList;
+    typedef std::map<int, Channel *> ChannelMap;
 
-  EventLoop* ownerLoop_;
-  int epollfd_;
-  EventList events_;
-  ChannelMap channels_;
+    EventLoop *ownerLoop_;
+    int epollfd_;
+    EventList events_;
+    ChannelMap channels_;
 };
 
-}
-#endif  // MUDUO_NET_EPOLLER_H
+} // namespace muduo
+#endif // MUDUO_NET_EPOLLER_H
